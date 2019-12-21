dc12bb1131f9176dd95b8b53cd52fc4b94688d8f
Ben Anderson <dataknut@icloud.com>
2019-12-21 23:25:09 +0000

    code for testing openair & AURN download

---
+++ dataProcessing/getAURNData.R
@@ -0,0 +1,50 @@
+# in which we download Southampton hourly air quality data
+# or any others we choose using
+# importAURN from http://davidcarslaw.github.io/openair/
+
+library(openair)
+library(data.table)
+library(dkUtils)
+library(skimr)
+library(lubridate)
+library(ggplot2)
+library(plotly)
+
+# sites in AURN speak:
+# UKA00613 : A33 https://uk-air.defra.gov.uk/networks/site-info?site_id=SA33&view=View
+
+dataPath <- path.expand("~/Data/SCC/airQual/")
+#"2016", "2017", "2018", 
+years <- c("2019")
+
+dfW <- openair::importAURN(
+  site = "SA33",
+  year = 2019,
+  pollutant = "all",
+  hc = FALSE,
+  meta = TRUE,
+  to_narrow = FALSE, # produces long form data yay!
+  verbose = TRUE # for now
+)
+
+dfL <- openair::importAURN(
+  site = "SA33",
+  year = 2019,
+  pollutant = "all",
+  hc = FALSE,
+  meta = TRUE,
+  to_narrow = TRUE, # produces long form data yay!
+  verbose = FALSE
+)
+
+dtL <- data.table::as.data.table(dfL) # we like data.tables
+
+openair::windRose(dfW) # only works with wide form
+openair::pollutionRose(dfW, pollutant = "no") # only works with wide form
+openair::calendarPlot(dfW, pollutant = "no")
+
+
+# ws (wind speed)
+# wd (wind direction) 
+
+metaDT <- data.table::as.data.table(openair::importMeta())
