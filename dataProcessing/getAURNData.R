db9f08be84989bcc0805db68e975160f6918f8d4
Ben Anderson <dataknut@icloud.com>
2020-04-06 11:24:48 +0100

    fixed to save year files and get 2019 (was missing!)

--- dataProcessing/getAURNData.R
+++ dataProcessing/getAURNData.R
@@ -17,13 +17,15 @@ library(lubridate)
 # Data cookery: https://uk-air.defra.gov.uk/assets/documents/Data_Validation_and_Ratification_Process_Apr_2017.pdf
 dataPath <- path.expand("~/Data/SCC/airQual/aurn/")
 #"2016", "2017", "2018", 
-years <- c("2016", "2017", "2018","2020")
+years <- c("2016", "2017", "2018","2019","2020")
 sites <- c("SA33", "SOUT")
 
 # get data ----
-allDT <- data.table::data.table()
+lyearDT <- data.table::data.table()
+wyearDT <- data.table::data.table()
 
 for(y in years){
+  # long form
   for(s in sites){
     df <- openair::importAURN(
       site = s,
@@ -33,16 +35,31 @@ for(y in years){
       to_narrow = TRUE, # produces long form data yay!
       verbose = TRUE
     )
-    dt <- data.table::as.data.table(df)
-    allDT <- rbind(dt, allDT)
+    ldt <- data.table::as.data.table(df)
+    lyearDT <- rbind(ldt, lyearDT)
   }
+  of <- paste0(dataPath,"processed/AURN_SSC_sites_hourlyAirQual_processed_", y,"_long.csv")
+  data.table::fwrite(lyearDT, file = of)
+  dkUtils::gzipIt(of)
+  
+  # wide form
+  for(s in sites){
+    df <- openair::importAURN(
+      site = s,
+      year = y,
+      pollutant = "all",
+      hc = FALSE,
+      to_narrow = FALSE, # produces wide form data
+      verbose = TRUE
+    )
+    dt <- data.table::as.data.table(df)
+    wyearDT <- rbind(dt, wyearDT, fill = TRUE) # different number of columns
+  }
+  of <- paste0(dataPath,"processed/AURN_SSC_sites_hourlyAirQual_processed_", y,"_wide.csv")
+  data.table::fwrite(wyearDT, file = of)
+  dkUtils::gzipIt(of)
+  wyearDT <- NULL
+  lyearDT <- NULL
 }
-
-
-pf <- paste0(dataPath,"processed/AURN_SSC_sites_hourlyAirQual_processed.csv")
-data.table::fwrite(allDT, file = pf)
-dkUtils::gzipIt(pf)
-
-skimr::skim(allDT)
 
 # done ----
