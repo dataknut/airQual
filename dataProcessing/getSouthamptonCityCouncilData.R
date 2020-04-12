f47fd8f464a64453eced1631cd22fd03c9791df0
Ben Anderson <dataknut@icloud.com>
2020-04-12 19:07:23 +0100

    re-ran SSC data collection back to original

--- dataProcessing/getSouthamptonCityCouncilData.R
+++ dataProcessing/getSouthamptonCityCouncilData.R
@@ -18,22 +18,22 @@ library(lubridate)
 
 # > years ----
 #"2016", "2017", "2018", 
-years <- c("2020")
+years <- c("2016","2017","2018","2019","2020")
 upTo <- lubridate::today()
 
 # > locations ----
 locs <- list()
 # sites: SH0 - 5 <- Southampton
 # SH0 A33 Roadside AURN
-locs$sh0 <- "Southampton - A33 Roadside (near docks, AURN site)"
+locs$sh0 <- "Southampton - A33 Roadside\n(near docks, AURN site)"
 # SH1 Southampton Background AURN
-locs$sh1 <- "Southampton - Background (near city centre, AURN site)"
+locs$sh1 <- "Southampton - Background\n(near city centre, AURN site)"
 locs$sh2 <- "Southampton - Redbridge"
 # SH3 Onslow Road
-locs$sh3 <- "Southampton - Onslow Road (near RSH)"
+locs$sh3 <- "Southampton - Onslow Road\n(near RSH)"
 locs$sh4 <- "Southampton - Bitterne"
 # SH5 Victoria Road
-locs$sh5 <- "Southampton - Victoria Road (Woolston)"
+locs$sh5 <- "Southampton - Victoria Road\n(Woolston)"
 
 #sites: <- Eastleigh
 # ES1 Southampton Road
@@ -46,7 +46,7 @@ locs$es3 <- "Eastleigh - The Point (Centre)"
 baseUrl <- "http://southampton.my-air.uk/singlela/relay/access.php?data=graph-data"
 dataPath <- path.expand("~/Data/SCC/airQual/direct/")
 
-dataDT <- data.table::data.table() # data bucket
+metaDT <- data.table::data.table() # meta data bucket
 
 # Functions ----
 refreshData <- function(years){
@@ -67,16 +67,18 @@ refreshData <- function(years){
       
       if(s == 0){
         # the column names are specific to the site so can't loop easily :-(
-        message("Location: ", locs$sh0)
+        loc <- locs$sh0
+        message("Location: ", loc)
         dt[, no := `Southampton - A33 Roadside AURN: Nitric Oxide (ug/m3)`]
         dt[, no2 := `Southampton - A33 Roadside AURN: Nitrogen Dioxide (ug/m3)`]
         dt[, nox := `Southampton - A33 Roadside AURN: Oxides of Nitrogen (ug/m3)`]
         dt[, pm10 := `Southampton - A33 Roadside AURN: PM10 Particulate (ug/m3)`]
-        dt[, site := locs$sh0]
+        dt[, site := loc]
         dt <- dt[, .(site, MeasurementDateGMT, no, no2, nox, pm10)]
       }
       if(s == 1){
-        message("Location: ", locs$sh1)
+        loc <- locs$sh1
+        message("Location: ", loc)
         dt[, co := `Southampton Background AURN: Carbon Monoxide (mg/m3)`]
         dt[, no := `Southampton Background AURN: Nitric Oxide (ug/m3)`]
         dt[, no2 := `Southampton Background AURN: Nitrogen Dioxide (ug/m3)`]
@@ -85,11 +87,12 @@ refreshData <- function(years){
         dt[, pm10 := `Southampton Background AURN: PM10 Particulate (ug/m3)`]
         dt[, pm2_5 := `Southampton Background AURN: PM2.5 Particulate (ug/m3)`]
         dt[, so2 := `Southampton Background AURN: Sulphur Dioxide (ug/m3)`]
-        dt[, site := locs$sh1]
+        dt[, site := loc]
         dt <- dt[, .(site, MeasurementDateGMT, co, no, no2, nox, oz, pm10, pm2_5, so2)]
       }
       if(s == 2){
-        message("Location: ", locs$sh2)
+        loc <- locs$sh2
+        message("Location: ", loc)
         #dt[, co := `Southampton Background AURN: Carbon Monoxide (mg/m3)`]
         dt[, no := `Southampton - Redbridge: Nitric Oxide (ug/m3)`]
         dt[, no2 := `Southampton - Redbridge: Nitrogen Dioxide (ug/m3)`]
@@ -98,11 +101,12 @@ refreshData <- function(years){
         dt[, pm10 := `Southampton - Redbridge: PM10 Particulate (ug/m3)`]
         #dt[, pm2_5 := `Southampton Background AURN: PM2.5 Particulate (ug/m3)`]
         dt[, so2 := `Southampton - Redbridge: Sulphur Dioxide (ug/m3)`]
-        dt[, site := locs$sh2]
+        dt[, site := loc]
         dt <- dt[, .(site, MeasurementDateGMT, no, no2, nox, oz, pm10, so2)]
       }
       if(s == 3){
-        message("Location: ", locs$sh3)
+        loc <- locs$sh3
+        message("Location: ", loc)
         #dt[, co := `Southampton Background AURN: Carbon Monoxide (mg/m3)`]
         dt[, no := `Southampton - Onslow Road: Nitric Oxide (ug/m3)`]
         dt[, no2 := `Southampton - Onslow Road: Nitrogen Dioxide (ug/m3)`]
@@ -111,11 +115,12 @@ refreshData <- function(years){
         #dt[, pm10 := `Southampton - Onslow Road: PM10 Particulate (ug/m3)`]
         #dt[, pm2_5 := `Southampton Background AURN: PM2.5 Particulate (ug/m3)`]
         #dt[, so2 := `Southampton - Onslow Road: Sulphur Dioxide (ug/m3)`]
-        dt[, site := locs$sh3]
+        dt[, site := loc]
         dt <- dt[, .(site, MeasurementDateGMT, no, no2, nox)]
       }
       if(s == 4){
-        message("Location: ", locs$sh4)
+        loc <- locs$sh4
+        message("Location: ", loc)
         #dt[, co := `Southampton Background AURN: Carbon Monoxide (mg/m3)`]
         dt[, no := `Southampton - Bitterne: Nitric Oxide (ug/m3)`]
         dt[, no2 := `Southampton - Bitterne: Nitrogen Dioxide (ug/m3)`]
@@ -124,11 +129,12 @@ refreshData <- function(years){
         dt[, pm10 := `Southampton - Bitterne: PM10 Particulate (ug/m3)`]
         #dt[, pm2_5 := `Southampton Background AURN: PM2.5 Particulate (ug/m3)`]
         dt[, so2 := `Southampton - Bitterne: Sulphur Dioxide (ug/m3)`]
-        dt[, site := locs$sh4]
+        dt[, site := loc]
         dt <- dt[, .(site, MeasurementDateGMT, no, no2, nox, pm10, so2)]
       }
       if(s == 5){
-        message("Location: ", locs$sh5)
+        loc <- locs$sh5
+        message("Location: ", loc)
         #dt[, co := `Southampton Background AURN: Carbon Monoxide (mg/m3)`]
         dt[, no := `Southampton - Victoria Road: Nitric Oxide (ug/m3)`]
         dt[, no2 := `Southampton - Victoria Road: Nitrogen Dioxide (ug/m3)`]
@@ -137,28 +143,26 @@ refreshData <- function(years){
         #dt[, pm10 := `Southampton - Victoria Road: PM10 Particulate (ug/m3)`]
         #dt[, pm2_5 := `Southampton Background AURN: PM2.5 Particulate (ug/m3)`]
         #dt[, so2 := `Southampton - Victoria Road: Sulphur Dioxide (ug/m3)`]
-        dt[, site := locs$sh5]
+        dt[, site := loc]
         dt[, .(site, MeasurementDateGMT, no, no2, nox)]
       }
       dt[, dateTimeUTC := lubridate::ymd_hm(MeasurementDateGMT)] # makes life so much easier later
       pf <- paste0(dataPath,"processed/", y, "_SSC_site_", s, "_hourlyAirQual_processed.csv")
       data.table::fwrite(dt, file = pf)
       dkUtils::gzipIt(pf)
+      meta <- dt[, .(startDate = min(dateTimeUTC),
+                     endDate = max(dateTimeUTC),
+                     site = loc,
+                     year = y)]
+      metaDT <- rbind(metaDT, meta)
     }
-    dataDT <- rbind(dataDT, dt, fill = TRUE)
   }
-  return(dataDT)
+  return(metaDT)
 }
 
-sotonRawDT <- refreshData(years) # only returns the last one downloaded
-
-summary(sotonRawDT[!is.na(no2)])
-
-# Last data entry:
-lastData <- max(sotonRawDT[!is.na(no2), (dateTimeUTC)])
-lastData
-
-now() - lastData
+# code ----
+metaDT <- refreshData(years = years)
 
 # done ----
-        
+# check what arrived
+metaDT
