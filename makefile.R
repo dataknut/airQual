341727850e3da6b7935eda9f7d53181a692d8bc8
Ben Anderson <dataknut@icloud.com>
2020-04-18 14:24:52 +0100

    added makefile to control everything

---
+++ makefile.R
@@ -0,0 +1,8 @@
+# make file. Makes everything
+
+# refresh the data
+source("dataProcessing/getAURNData.R")
+source("dataProcessing/getSouthamptonCityCouncilData.R")
+
+# run the lockdown analysis
+source("rmd/make_SotonLockdown.R")
