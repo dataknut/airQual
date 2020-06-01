881006599fcfa6a38b67bf24e9e019130820857f
B.Anderson <ba1e12@srv02405.soton.ac.uk>
2020-06-01 12:18:21 +0100

    added some clean-up to save memory

--- makefile.R
+++ makefile.R
@@ -7,5 +7,10 @@ source("dataProcessing/getAURNData.R")
 #source("dataProcessing/getSouthamptonCityCouncilData.R")
 source("dataProcessing/getSouthamptonHantsAir.R")
 
+tablesToDelete <- data.table::tables()
+l <- tablesToDelete[, NAME]
+remove(list = l) # clear memory. Each script really ought to do this for itself
+
 # run the lockdown analysis
+# if you get memory errors restart R here, the rmarkdown::render() can fail
 source("rmd/make_SotonLockdown.R")
