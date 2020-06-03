8db2d1fd84f1e097ba11ea7868cd06d4abf6550d
B.Anderson <ba1e12@srv02405.soton.ac.uk>
2020-06-03 21:17:33 +0100

    rebuilt package. Still mnot sure why the annotation functions don't work inside other functions.

--- R/compareYearsDiffPlotWeekly.R
+++ R/compareYearsDiffPlotWeekly.R
@@ -17,8 +17,7 @@
 compareYearsDiffPlotWeekly <- function(dt, ldStart, ldEnd){
   # final plot - adds annotations
   # use means for consistency with comparison plots where we use WHO thresholds (means)
-  yMin <- min(dt$pcDiffMean)
-  yMax <- max(dt$pcDiffMean)
+  
   p <- ggplot2::ggplot(dt, aes(x = weekNo, y = pcDiffMean)) +
     geom_step() +
     theme(axis.text.x=element_text(angle=90, hjust=1)) +
@@ -26,19 +25,9 @@ compareYearsDiffPlotWeekly <- function(dt, ldStart, ldEnd){
          y = "% difference 2020 vs 2017-2019"
     ) +  
     theme(legend.position="bottom") +
-    geom_hline(yintercept = 0,linetype = 3)
-  # lubridate::week(as.Date("2020-03-24"))
-  p <- p + annotate("rect", xmin = (lubridate::week(ldStart) - 0.1),
-                    xmax = (lubridate::week(ldEnd) + 0.1), 
-                    ymin = yMin - 1, ymax = yMax + 1, 
-                    alpha = myParams$myAlpha, 
-                    fill = myParams$vLineCol, 
-                    colour = myParams$vLineCol)
-  #p <- addLockdownDate(p, yMin, yMax)
-  p <- p + facet_grid(site ~ .) +
+    geom_hline(yintercept = 0,linetype = 3) + 
+    facet_grid(site ~ .) +
     theme(strip.text.y.right = element_text(angle = 90))
-  #p <- addWeekendsDate(p, yMin, yMax) + scale_x_date(date_breaks = "7 day",
-  #                                             date_labels =  "%a %d %b",
-  #                                            date_minor_breaks = "1 day")
+
   return(p)
 }
